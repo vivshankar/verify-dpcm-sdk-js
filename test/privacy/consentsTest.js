b0b78ff1df3c5a754a5440bd61419d9c39648a30
Raghuram Kokalla <raghuram@sg.ibm.com>
2021-09-24 14:17:34 +0800

    support retrieval of consents for the current application

--- test/privacy/consentsTest.js
+++ test/privacy/consentsTest.js
@@ -3,7 +3,6 @@ const Env = require('../utils/config');
 const config = Env.Config; const auth = Env.Auth; const context = Env.Context;
 const checkConfig = Env.checkConfig;
 const Privacy = require('../../lib/privacy');
-const { stringify } = require('querystring');
 
 describe('Privacy', () => {
   before(async () => {
@@ -36,6 +35,22 @@ describe('Privacy', () => {
         const result = await client.getUserConsents();
         assert(result.status == 'done',
             `Result status is unexpected: ${result.status}`);
+      } catch (error) {
+        assert.fail(`Error thrown:\n${error}`);
+      }
+    });
+
+    it('get user consents for the current application', async () => {
+      const client = new Privacy(config, auth, context);
+
+      try {
+        const result = await client.getUserConsents(
+            {filterByCurrentApplication: true},
+        );
+        assert.notEqual(result.consents.length, 0);
+        for (const consent of result.consents) {
+          assert.strictEqual(!!consent.applicationId, true);
+        }
       } catch (error) {
         assert.fail(`Error thrown:\n${error}`);
       }
